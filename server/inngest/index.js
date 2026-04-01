051a526bf1b6af68594ba5c97c22e1f55567dd0f
Sapna44Y <yadavsapna2004@gmail.com>
2026-04-01 18:22:27 +0530

    "updated index.js code "

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,14 +6,16 @@ export const inngest = new Inngest({ id: "SocialUp" });
 
 //Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
-    let username = email_addresses[0].email_addresses.split("@")[0];
 
-    //check availability of username
+    let username = email_addresses[0].email_address.split("@")[0];
+
     const user = await User.findOne({ username });
 
     if (user) {
@@ -22,35 +24,42 @@ const syncUserCreation = inngest.createFunction(
 
     const userData = {
       _id: id,
-      email: email_addresses[0].email_addresses,
+      email: email_addresses[0].email_address,
       full_name: first_name + " " + last_name,
       profile_picture: image_url,
       username,
     };
+
     await User.create(userData);
   },
 );
 
 //Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-  { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
+
     const updatedUserData = {
-      email: email_addresses[0].email_addresses,
+      email: email_addresses[0].email_address,
       full_name: first_name + " " + last_name,
       profile_picture: image_url,
     };
+
     await User.findByIdAndUpdate(id, updatedUserData);
   },
 );
 
 //Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-  { id: "delete-user-with-clerk" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "delete-user-with-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
   async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete(id);
